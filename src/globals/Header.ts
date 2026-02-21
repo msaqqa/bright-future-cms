afb792e7a8a05b9149848297244887bd11d0b014
masaqqa <ma.saqqa@gmail.com>
2026-02-21 14:45:16 +0200

    fixed chaded issue

--- src/globals/Header.ts
+++ src/globals/Header.ts
@@ -1,8 +1,12 @@
+import { revalidateAll } from '@/hooks/revalidate'
 import { GlobalConfig } from 'payload'
 
 export const Header: GlobalConfig = {
   slug: 'header',
   label: 'Header Settings',
+  hooks: {
+    afterChange: [revalidateAll],
+  },
   admin: {
     group: 'Layout',
   },
