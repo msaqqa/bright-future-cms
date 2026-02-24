337ab99e3d56b73215658eefc40ae3db3bbd1b27
masaqqa <ma.saqqa@gmail.com>
2026-02-24 15:25:34 +0200

    restart name file to Elektroinstallation

--- src/globals/index.ts
+++ src/globals/index.ts
@@ -1,7 +1,7 @@
 import { Header } from './Header'
 import { Footer } from './Footer'
 import { Home } from './Home'
-import { Elektroinstallation } from './Temp'
+import { Elektroinstallation } from './Elektroinstallation'
 import { Photovoltaik } from './Photovoltaik'
 import { Energiespeicher } from './Energiespeicher'
 import { Balkonkraftwerke } from './Balkonkraftwerke'
